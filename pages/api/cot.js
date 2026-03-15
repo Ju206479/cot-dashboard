4c8db941abb353b5523e9191964b639fe594943e
Ju206479 <julienberetta@hotmail.fr>
2026-03-15 11:59:43 +0700

    Update cot.js

--- pages/api/cot.js
+++ pages/api/cot.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { code } = req.query;
   if (!code) return res.status(400).json({ error: "Missing code" });
 
-  const url = `https://publicreporting.cftc.gov/api/odata/v1/CorrectionsOtherFuturesCombined?%24select=Market_and_Exchange_Names%2C_Report_Date_as_YYYY_MM_DD%2COpen_Interest_All%2CNonComm_Positions_Long_All%2CNonComm_Positions_Short_All%2CChange_in_NonComm_Long_All%2CChange_in_NonComm_Short_All&%24filter=CFTC_Contract_Market_Code%20eq%20'${encodeURIComponent(code)}'&%24orderby=_Report_Date_as_YYYY_MM_DD%20desc&%24top=20`;
+  const url = `https://publicreporting.cftc.gov/api/odata/v1/CorrectionsOtherFuturesCombined?$select=Market_and_Exchange_Names,_Report_Date_as_YYYY_MM_DD,Open_Interest_All,NonComm_Positions_Long_All,NonComm_Positions_Short_All,Change_in_NonComm_Long_All,Change_in_NonComm_Short_All&$filter=CFTC_Contract_Market_Code eq '${code}'&$orderby=_Report_Date_as_YYYY_MM_DD desc&$top=20`;
 
   try {
     const response = await fetch(url);
